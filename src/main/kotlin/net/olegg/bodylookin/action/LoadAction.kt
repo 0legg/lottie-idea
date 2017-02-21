e9c72f288757d2053156d868b44fa28ca4c8a1b3
Oleg Godovykh <olegg.lieangel@gmail.com>
2017-02-21 01:08:39 -0800

    add load action

--- src/main/kotlin/net/olegg/bodylookin/action/LoadAction.kt
+++ src/main/kotlin/net/olegg/bodylookin/action/LoadAction.kt
@@ -2,33 +2,56 @@ package net.olegg.bodylookin.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.wm.ToolWindowManager
 import net.olegg.bodylookin.Constants
+import net.olegg.bodylookin.isJson
 import net.olegg.bodylookin.toolwindow.BodylookinView
 
 /**
  * Created by olegg on 2/21/17.
  */
-class LoadAction: AnAction() {
+class LoadAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
-        val project = event.project ?: return
-        val editor = FileEditorManager.getInstance(project).selectedTextEditor
+        val project = event.project
+        if (project == null || project.isDefault || project.isDisposed) {
+            return
+        }
+
+        val doc = event.getData(CommonDataKeys.EDITOR)?.document
+        val file = event.getData(CommonDataKeys.VIRTUAL_FILE)
         val toolWindowManager = ToolWindowManager.getInstance(project)
         val toolWindow = toolWindowManager.getToolWindow(Constants.TOOL_WINDOW_ID)
-        val json = editor?.document?.text
         if (toolWindow.isVisible) {
             val content = toolWindow.contentManager.getContent(0)?.component as? BodylookinView ?: return
-            if (json != null) {
-                content.loadJson(json)
+            if (doc != null) {
+                content.loadJson(doc.text)
+            } else if (file != null) {
+                content.loadFile(file)
             }
         } else {
             toolWindow.show {
                 val content = toolWindow.contentManager.getContent(0)?.component as? BodylookinView ?: return@show
-                if (json != null) {
-                    content.loadJson(json)
+                if (doc != null) {
+                    content.loadJson(doc.text)
+                } else if (file != null) {
+                    content.loadFile(file)
                 }
             }
         }
+    }
+
+    override fun update(e: AnActionEvent) {
+        val project = e.getData(CommonDataKeys.PROJECT)
+        if (project == null || project.isDefault || project.isDisposed) {
+            e.presentation.isEnabledAndVisible = false
+            return
+        }
+        val doc = e.getData(CommonDataKeys.EDITOR)?.document
+        val file = e.getData(CommonDataKeys.VIRTUAL_FILE)
+
+        val docJson = doc != null && doc.textLength > 0 && FileDocumentManager.getInstance().getFile(doc).isJson
+        e.presentation.isEnabledAndVisible = file.isJson || docJson
     }
 }
